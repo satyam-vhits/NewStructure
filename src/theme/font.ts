680c8150469517b25cdbcd6b641a84295c31a4d3
yashnadha <yash.vhits@gmail.com>
2023-11-29 10:04:04 +0530

    Link fonts

--- src/theme/font.ts
+++ src/theme/font.ts
@@ -1,1 +1,19 @@
-export default {};
+export default {
+  poppinsBlack: 'Poppins-Black',
+  poppinsBlackItalic: 'Poppins-BlackItalic',
+  poppinsBold: 'Poppins-Bold',
+  poppinsBoldItalic: 'Poppins-BoldItalic',
+  poppinsExtraBold: 'Poppins-ExtraBold',
+  poppinsExtraBoldItalic: 'Poppins-ExtraBoldItalic',
+  poppinsExtraLight: 'Poppins-ExtraLight',
+  poppins_talic: 'Poppins-Italic',
+  poppinsLight: 'Poppins-Light',
+  poppinsLightItalic: 'Poppins-LightItalic',
+  poppinsMedium: 'Poppins-Medium',
+  poppinsMediumItalic: 'Poppins-MediumItalic',
+  poppinsRegular: 'Poppins-Regular',
+  poppinsSemiBold: 'Poppins-SemiBold',
+  poppinsSemiBoldItalic: 'Poppins-SemiBoldItalic',
+  poppinsThin: 'Poppins-Thin',
+  poppinsThinItalic: 'Poppins-ThinItalic',
+};
