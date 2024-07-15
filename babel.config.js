06989a93013581da837ba2b49f3b9b3f88c9c64f
Yash Nandha <92366452+Yashnandha@users.noreply.github.com>
2024-07-15 17:36:11 +0530

    Fix babel issue in 0.74.3

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
+
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  overrides: [{
+    "plugins": [
+      ["@babel/plugin-transform-private-methods", {
+      "loose": true
+    }]
+    ]
+  }],
   plugins: [
     // ... other configs, if an
     [
