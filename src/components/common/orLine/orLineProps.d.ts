b5656bfc11c5f52c47d5501c4ea243dae81d8bcc
Yash Nandha <92366452+Yashnandha@users.noreply.github.com>
2024-07-31 18:47:06 +0530

    Bump Code

--- src/components/common/orLine/orLineProps.d.ts
+++
@@ -1,8 +0,0 @@
-import {TextStyle} from 'react-native';
-
-interface OrLineProps {
-  label?: string | undefined;
-  lineColor?: string | undefined;
-  labelStyle?: TextStyle | undefined;
-  lineHorizontalSpace?: number | undefined;
-}
--- src/components/componentsIndex.ts
+++ src/components/componentsIndex.ts
@@ -1,6 +1,4 @@
 import CustomStatusbar from './common/customStatusbar/CustomStatusbar';
-import InputContainer from './common/inputContainer/InputContainer';
-import Button from './common/Button/Button';
-import OrLine from './common/orLine/OrLine';
+import Button from './common/button/Button';
 
-export {CustomStatusbar, InputContainer, Button, OrLine};
+export {CustomStatusbar, Button};
